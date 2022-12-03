a63c590833a93829428ee08fce22501fa1c51822
TheScendant <hayden518@gmail.com>
2022-12-02 17:47:45 -0700

    recharts graph

---
+++ src/RechartsGraph.tsx
@@ -0,0 +1,42 @@
+import * as d3 from 'd3';
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { BarChart, Bar, XAxis, YAxis, 
+  CartesianGrid } from 'recharts';
+import { sliceData } from './GradeSlicer';
+import { TimeSliceEnum } from './TimeSlicer';
+import { redScaleArray } from './utils';
+export const RechartsGraph = () => {
+  const timeSlice = TimeSliceEnum.YEAR;
+
+  const sends = useSelector((state: any) => state.sendsData.sends) || [];
+
+  const [agg, setAgg] = useState(true);
+  const [canShowModal, setCanShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalData, setModalData] = useState({});
+  //@ts-ignore
+  const [gradeDateQuantityArray, years] = sliceData(sends, timeSlice, agg);
+  const data = gradeDateQuantityArray;
+
+  for (const d of data) {
+    let total = 0;
+    for (let k = 0; k < years.length; k++) {
+      const year = years[k];
+      total += d[year] ? d[year] : 0;
+    }
+    d.total = total;
+  }
+
+  return (
+    <BarChart width={1000} height={1000} data={data} >
+      <CartesianGrid />
+      <XAxis dataKey="year" />
+      <YAxis />
+      {years.map((year: any, i: any) => (
+        // @ts-ignore
+        <Bar dataKey={year} stackId="a" fill={redScaleArray[i+3]} />
+      ))}
+    </BarChart>
+  )
+}
